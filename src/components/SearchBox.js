1921f9e7d15c7e7d3e18e1b5c160897382aad308
Tomás Plevak <tomiplevak@hotmail.com>
2022-11-09 19:36:46 -0300

    Debounce Search Feature added

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
+import { debounce } from 'lodash';
 
-import './SearchBox.css';
+import "./SearchBox.css";
 
 const SearchBox = (props) => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
-  const handdleChange = (event) => {
-    event.preventDefault();
-    setInput(event.target.value)
+  const handdleChange = (searchString) => {
+    setInput(searchString);
+    debounceSearch(searchString);
   };
+
+  const debounceSearch = useMemo(() => {
+    return debounce((searchString) => props.onSearch(searchString), 500);
+  }, [props]);
 
   return (
     <div className="input-group mb-3 mx-auto">
@@ -18,7 +23,7 @@ const SearchBox = (props) => {
         placeholder="Search a Star Wars Character..."
         aria-label="Search a Star Wars Character..."
         aria-describedby="button-addon2"
-        onChange={handdleChange}
+        onChange={(e)=>handdleChange(e.target.value)}
         value={input}
       />
       <button
