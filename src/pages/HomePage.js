35c26f560023c43848d94355b1651be7c9486902
Tomás Plevak <tomiplevak@hotmail.com>
2022-11-09 23:32:36 -0300

    Name of pages changed

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CardsContainer from "../components/CardsContainer";
+
+const Home = () => {
+  return (
+    <>
+      <CardsContainer />
+    </>
+  );
+};
+
+export default Home;
